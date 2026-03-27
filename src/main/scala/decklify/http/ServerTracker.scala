febcc44ac44465682d93eb3563bdb6bd5afbaa16
ethan <ethansoya@gmail.com>
2026-03-27 20:03:49 +0100

    fix: use polling instead of countlatch

--- src/main/scala/decklify/http/ServerTracker.scala
+++ src/main/scala/decklify/http/ServerTracker.scala
@@ -1,14 +1,11 @@
 package decklify.http
 
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicReference
 import javax.jmdns.ServiceEvent
 import javax.jmdns.ServiceListener
 
 object ServerTracker extends ServiceListener {
   private val serverUrl = new AtomicReference[Option[String]](None)
-  private val serverFound = CountDownLatch(1)
 
   override def serviceAdded(event: ServiceEvent): Unit = ()
 
@@ -18,7 +15,6 @@ object ServerTracker extends ServiceListener {
     val port = info.getPort
     serverUrl.set(Some(s"http://$ip:$port"))
     println(s"Server online: ${serverUrl.get.toString}")
-    serverFound.countDown
 
   override def serviceRemoved(event: ServiceEvent): Unit =
     println("Server went offline")
@@ -27,5 +23,9 @@ object ServerTracker extends ServiceListener {
   def getUrl: Option[String] = serverUrl.get
 
   def waitForServer(error: () => Unit): Unit =
-    if !serverFound.await(10, TimeUnit.SECONDS) then error(): Unit
+    val deadline = System.currentTimeMillis() + 10000
+    while serverUrl.get.isEmpty && System.currentTimeMillis() < deadline do
+      Thread.sleep(500)
+    if serverUrl.get.isEmpty then error()
+
 }
